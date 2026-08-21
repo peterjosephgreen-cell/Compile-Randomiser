3a16547c04d443083f7714262e6fedc3f20b52ff
peterjosephgreen-cell <peterjosephgreen@hotmail.com>
2026-08-21 15:29:59 +1000

    Add files via upload

--- app.js
+++ app.js
@@ -620,7 +620,7 @@ const allProtocols = Object.entries(protocolSets).flatMap(([set, names]) =>
 );
 
 
-const APP_VERSION = "21.2.1";
+const APP_VERSION = "21.2.2";
 
 const protocolPlaystyles = {
   Darkness: "Manipulates face-down cards and hidden information. Strong when you can build value while denying the opponent certainty.",
